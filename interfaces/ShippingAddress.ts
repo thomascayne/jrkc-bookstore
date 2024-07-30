b6df529e15bd9f660ba920d7ce2653ff7bc37692
Thomas Cayne <thomas.cayne@slashand.com>
2024-07-30 08:41:24 -0500

    add ShippingAddress interface

---
+++ interfaces/ShippingAddress.ts
@@ -0,0 +1,13 @@
+// interfaces/ShippingAddress.ts
+
+export interface ShippingAddress {
+    shipping_first_name: string;
+    shipping_last_name: string;
+    shipping_street_address1: string;
+    shipping_street_address2: string;
+    shipping_city: string;
+    shipping_state: string;
+    shipping_zipcode: string;
+    shipping_country: string;
+    shipping_phone: string;
+}
