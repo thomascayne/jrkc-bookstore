82757ca0f9e96d3e7dd4d0d6b76370e9ef1465ad
Thomas Cayne <thomas.cayne@slashand.com>
2024-07-30 08:41:41 -0500

    add BillingAddress interface

---
+++ interfaces/BillingAddress.ts
@@ -0,0 +1,11 @@
+// interfaces/BillingAddress.ts
+
+export interface BillingAddress {
+    street_address1: string;
+    street_address2: string;
+    city: string;
+    state: string;
+    zipcode: string;
+    country: string;
+    phone: string;
+}
