2faccb8c28f049bd101a664b77929f7455753ed9
Thomas Cayne <thomas.cayne@slashand.com>
2024-07-31 15:51:11 -0500

    update with different pattern

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        domains: ['books.google.com'],
         remotePatterns: [
             {
                 protocol: 'https',
                 hostname: 'books.google.com',
-                pathname: '/books/content/**',
+                pathname: '**',
             },
         ],
     },
