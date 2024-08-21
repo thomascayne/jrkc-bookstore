0fac4d702f32e67435c2a310a1fa4edb54f1c54d
JayCastle6 <Joshua.Castillo15@student.ctuonline.edu>
2024-08-21 14:28:45 -0400

    Customer info partially working

--- app/sales/customer-info/page.tsx
+++ app/sales/customer-info/page.tsx
@@ -10,12 +10,15 @@ interface Customer {
   id: string;
   first_name: string;
   last_name: string;
+<<<<<<< HEAD
   phone: string;
   email: string;
   shipping_street_address1: string;
   shipping_city: string;
   shipping_state: string;
   shipping_zipcode: string;
+=======
+>>>>>>> c6ce44c (Customer info partially working)
 }
 
 export default function CustomerInfoPage() {
@@ -52,12 +55,15 @@ export default function CustomerInfoPage() {
           <TableColumn>ID</TableColumn>
           <TableColumn>First Name</TableColumn>
           <TableColumn>Last Name</TableColumn>
+<<<<<<< HEAD
           <TableColumn>Phone</TableColumn>
           <TableColumn>Email</TableColumn>
           <TableColumn>Shipping Address</TableColumn>
           <TableColumn>City</TableColumn>
           <TableColumn>State</TableColumn>
           <TableColumn>Zip Code</TableColumn>
+=======
+>>>>>>> c6ce44c (Customer info partially working)
         </TableHeader>
         <TableBody>
           {customers.map((customer) => (
@@ -65,12 +71,15 @@ export default function CustomerInfoPage() {
               <TableCell>{customer.id}</TableCell>
               <TableCell>{customer.first_name}</TableCell>
               <TableCell>{customer.last_name}</TableCell>
+<<<<<<< HEAD
               <TableCell>{customer.phone}</TableCell>
               <TableCell>{customer.email}</TableCell>
               <TableCell>{customer.shipping_street_address1}</TableCell>
               <TableCell>{customer.shipping_city}</TableCell>
               <TableCell>{customer.shipping_state}</TableCell>
               <TableCell>{customer.shipping_zipcode}</TableCell>
+=======
+>>>>>>> c6ce44c (Customer info partially working)
             </TableRow>
           ))}
         </TableBody>
